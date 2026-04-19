a83676b773e4f6ae65cc1b3014663bf5b7af5175
LoweDavid02 <louiecamarines21@gmail.com>
2026-04-19 23:02:14 +0800

    Some issues fixed

--- LARAVEL-BACK-END/routes/web.php
+++ LARAVEL-BACK-END/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Health check endpoint for Render
+Route::get('/up', function () {
+    return response()->json([
+        'status' => 'ok',
+        'timestamp' => now()->toIso8601String(),
+    ]);
+});
